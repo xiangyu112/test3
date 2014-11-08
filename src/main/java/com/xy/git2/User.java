4dca2e56f1f0422b33ef1bc1b04991bff72afde2
xiangyu112 <58190192@qq.com>
2014-11-08 13:02:53 +0800

    1

--- src/main/java/com/xy/git2/User.java
+++ src/main/java/com/xy/git2/User.java
@@ -7,6 +7,9 @@ public class User {
 	 */
 	public static void main(String[] args) {
 		System.out.println("user");
+		System.out.println("user");
+		System.out.println("user");
+		System.out.println("user");
 
 	}
 
